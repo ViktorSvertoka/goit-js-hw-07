f53ef0f2d3126519b82999e06b683e6f7fb1036e
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-18 23:35:53 +0200

    Task-7 gallery fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -35,8 +35,18 @@ function selectGalleryEl(event) {
   const instance = basicLightbox.create(
     `<img src="${event.target.dataset.source}" width="800" height="600" style= "border-radius: 5%; box-shadow: inset 2px 2px 2px 0px rgba(255, 255, 255, 0.5),
     7px 7px 20px 0px rgba(0, 0, 0, 0.1), 4px 4px 5px 0px rgba(0, 0, 0, 0.1)">`,
+
+    {
+      onShow: () => {
+        window.addEventListener('keydown', onKeydownEsc);
+      },
+      onClose: () => {
+        window.removeEventListener('keydown', onKeydownEsc);
+      },
+    },
   );
-  instance.show();
+
+  // instance.show();
 
   const onKeydownEsc = event => {
     console.log(event.code);
@@ -44,6 +54,8 @@ function selectGalleryEl(event) {
       instance.close();
     }
   };
+  // window.addEventListener('keydown', onKeydownEsc);
+  // window.removeEventListener('keydown', onKeydownEsc);
 
-  window.addEventListener('keydown', onKeydownEsc);
+  instance.show();
 }
