693e51451f53263ae491890f9e84fa91b1bddabd
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-02-22 00:37:28 +0200

    Update comments

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,7 @@
 import { galleryItems } from './gallery-items.js';
 
+//! Change code below this line
+
 const galleryContainer = document.querySelector('.gallery');
 const galleryCardsSet = createGallery(galleryItems);
 
